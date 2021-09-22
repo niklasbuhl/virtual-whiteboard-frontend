98c90982321dff413f42a2dccaffc130d350d5b9
Niklas Buhl <hello@niklasbuhl.com>
2021-09-22 22:10:14 +0200

    toolbox appearance

--- src/components/fabric/toolbox.tsx
+++ src/components/fabric/toolbox.tsx
@@ -1,5 +1,5 @@
 import { ToolContext, Tool } from "../../context/toolContext"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { AuthContext } from "../../context/authContextProvider"
 
 export type Props = {}
@@ -7,42 +7,60 @@ export type Props = {}
 export const ToolBox: React.FC<Props> = () => {
 	const { tool, setTool } = useContext(ToolContext)
 	const { loggedIn } = useContext(AuthContext)
+	const [drawBtnCSS, setDrawBtnCSS] = useState("btn m-2")
+	const [editBtnCSS, setEditBtnCSS] = useState("btn m-2")
+
+	useEffect(() => {
+		if (tool === Tool.Draw) setDrawBtnCSS("btn btn-secondary m-2")
+		else setDrawBtnCSS("btn m-2")
+	}, [tool])
+
+	useEffect(() => {
+		if (tool === Tool.Edit) setEditBtnCSS("btn btn-secondary m-2")
+		else setEditBtnCSS("btn m-2")
+	}, [tool])
 
 	return (
 		<div className="d-flex">
 			<button
 				disabled={loggedIn === false}
-				className="btn button-primary"
+				type="button"
+				className="btn m-2"
 				onClick={() => setTool(Tool.NewText)}>
 				New Text
 			</button>
 			<button
 				disabled={loggedIn === false}
-				className="btn button-primary"
+				type="button"
+				className="btn m-2"
 				onClick={() => setTool(Tool.NewImage)}>
 				New Image from URL
 			</button>
 
 			<button
-				className="btn button-primary"
+				className={drawBtnCSS}
+				type="button"
 				disabled={tool === Tool.Draw || loggedIn === false}
 				onClick={() => setTool(Tool.Draw)}>
 				Draw
 			</button>
 			<button
-				className="btn button-primary"
+				className={editBtnCSS}
+				type="button"
 				disabled={tool === Tool.Edit}
 				onClick={() => setTool(Tool.Edit)}>
 				Edit
 			</button>
 			<button
-				className="btn button-primary"
+				className="btn text-danger m-2"
+				type="button"
 				disabled={tool === Tool.Delete || loggedIn === false}
 				onClick={() => setTool(Tool.Delete)}>
 				Delete
 			</button>
 			<button
-				className="btn button-primary"
+				className="btn m-2"
+				type="button"
 				onClick={() => setTool(Tool.Refresh)}>
 				Refresh
 			</button>
